f61f7b99f6e48d6d0646060b2ebaaf6e9ae71954
kingkayin <130321880+kingkayin@users.noreply.github.com>
2024-03-28 23:24:52 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#更改默认地址为192.168.6.1
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+#更改默认地址为10.10.19.1
+sed -i 's/192.168.1.1/10.10.19.1/g' package/base-files/files/bin/config_generate
